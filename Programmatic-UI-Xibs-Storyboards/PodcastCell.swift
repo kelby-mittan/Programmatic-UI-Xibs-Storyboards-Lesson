213d3628a18684a099bea2549c1caaec357f4ce1
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-29 15:05:10 -0500

    Initial Commit

---
+++ Programmatic-UI-Xibs-Storyboards/PodcastCell.swift
@@ -0,0 +1,13 @@
+//
+//  PodcastCellCollectionViewCell.swift
+//  Programmatic-UI-Xibs-Storyboards
+//
+//  Created by Kelby Mittan on 1/29/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class PodcastCell: UICollectionViewCell {
+    
+}
